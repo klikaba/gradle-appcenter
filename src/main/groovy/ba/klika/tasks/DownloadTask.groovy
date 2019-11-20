2d45944f758f04937871a3c574e60de8635bcb98
Greitbauer, Philipp <philipp.greitbauer@bawagpsk.com>
2019-11-20 13:44:47 +0100

    insert verstion and buildnumber into filename, check for existing file

--- src/main/groovy/ba/klika/tasks/DownloadTask.groovy
+++ src/main/groovy/ba/klika/tasks/DownloadTask.groovy
@@ -1,15 +1,19 @@
 package ba.klika.tasks
 
 
-import groovy.util.Proxy
 import groovyx.net.http.RESTClient
+import org.apache.http.HttpStatus
 import org.gradle.api.GradleException
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.TaskAction
 
-import java.net.Proxy
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.nio.file.StandardCopyOption
+import java.time.Instant
 
 class DownloadTask extends AppcenterBaseTask {
     /**
@@ -24,6 +28,7 @@ class DownloadTask extends AppcenterBaseTask {
     /**
      * This is NOT the appcenters build ID.
      * At some appcenter endpoints it is named "buildNumber", on some other its "version" (whereas "short_version" is something like "2.6.0")
+     * From Appcenter Swagger: The release's version.<br>\nFor iOS: CFBundleVersion from info.plist.<br>\nFor Android: android:versionCode from AppManifest.xml.\n
      */
     @Input
     @Optional
@@ -38,35 +43,39 @@ class DownloadTask extends AppcenterBaseTask {
 
     /**
      * short_version like "2.6.0"
+     * From appcenter swagger: The release's short version.<br>\nFor iOS: CFBundleShortVersionString from info.plist.<br>\nFor Android: android:versionName from AppManifest.xml.\n",
+     *
+     * if only version is specified get latest matching build (don't rely on order of data)
+     * if also build number is given match on both
      */
     @Input
     @Optional
-    final Property<String> version = project.objects.property(String)
+    final Property<String> releaseVersion = project.objects.property(String)
 
     @Input
     final Property<String> distributionGroup = project.objects.property(String)
+
     @Input
     final Property<String> outPath = project.objects.property(String)
 
     @TaskAction
     def download() {
-        println "download task"
-
-        if(releaseId.orNull){
+        if(releaseId.present){
             downloadWithReleaseId(releaseId.get())
         }
         downloadWithoutReleaseId()
     }
 
     def downloadWithReleaseId(String releaseId){
-        println "downloadWithReleaseId"
-        String url=getDownloadUrl("/apps/${ownerName.get()}/${appName.get()}/releases/${releaseId}")
-        downloadFollowingRedirect(url, outPath.get())
+        println "downloadWithReleaseId ${releaseId}"
+        def data=getData("/apps/${ownerName.get()}/${appName.get()}/releases/${releaseId}")
+        downloadFollowingRedirect(data.download_url, outPath.get(), data.short_version, data.version)
     }
 
     def downloadWithoutReleaseId() {
         println "downloadWithoutReleaseId"
 
+        /*  this is redundant as @Input already checks for presence
         if (!distributionGroup.get()) {
             throw new GradleException("distributionGroup field must not be empty!")
         }
@@ -75,73 +84,94 @@ class DownloadTask extends AppcenterBaseTask {
         }
         if (!appName.get()) {
             throw new GradleException("appName field must no tbe empty!")
-        }
+        }*/
 
-        //use orNull because get throws exception
-        if(buildNumber.orNull || version.orNull){
+        if(buildNumber.present || releaseVersion.present){
             println "buildNumber or version specified"
+
             RESTClient client = getRestClient("/apps/${ownerName.get()}/${appName.get()}/distribution_groups/${distributionGroup.get()}/releases")
             def callResponse = catchHttpExceptions { client.get(headers()) }
-            String releaseId = null
-            if (callResponse.status == 200) {
+
+            if (callResponse.status == HttpStatus.SC_OK) {
                 def list = callResponse.getData()
-                println list
-                for (def entry in list){
-                    println "Checking entry for version/buildNumber ${entry}"
-                    if(buildNumber.orNull?.equals(entry.version) || version.orNull?.equals(entry.short_version)){
-                        releaseId=entry.id
-                        break
-                    }
-                }
-                if(!releaseId){
+                def release = getRelease(list, buildNumber.orNull, releaseVersion.orNull)
+
+                if(!release){
                     throw new GradleException("No release found")
                 }
-                downloadWithReleaseId(releaseId)
+                println (release)
+                downloadWithReleaseId(release.id as String)
             } else {
                 throw new GradleException("Error calling AppCenter. Status code " + callResponse.status + '\n' + callResponse.getData().toString())
             }
         } else {
-            String url=getDownloadUrl("/apps/${ownerName.get()}/${appName.get()}/distribution_groups/${distributionGroup.get()}/releases/latest")
-            downloadFollowingRedirect(url, outPath.get())
+            def data=getData("/apps/${ownerName.get()}/${appName.get()}/distribution_groups/${distributionGroup.get()}/releases/latest")
+            downloadFollowingRedirect(data.download_url, outPath.get(), data.short_version, data.version)
         }
     }
 
-    def getDownloadUrl(String endPoint){
-        println "getDownloadUrl"
+    static def getRelease(releaseList, String buildNumberString, String releaseVersionString) {
+        def release
+        if (buildNumberString) {
+            release = releaseList.find {
+                                    it.version == buildNumberString &&
+                                    (releaseVersionString==null || releaseVersionString == it.short_version) //check also for releaseVersion if present
+                                  }
+        } else {
+            //search for latest release of releaseVersion
+            release = releaseList.findAll { it.short_version == releaseVersionString }
+                    .max { Instant.parse(it.uploaded_at) }
+        }
+        return release
+    }
 
+    def getData(String endPoint){
         RESTClient client = getRestClient(endPoint)
         def callResponse = catchHttpExceptions { client.get(headers()) }
 
         if (callResponse.status == 200) {
-            return callResponse.getData().download_url
+            return callResponse.getData()
         } else {
             throw new GradleException("Error calling AppCenter. Status code " + callResponse.status + '\n' + callResponse.getData().toString())
         }
     }
 
-    def downloadFollowingRedirect(String url, String filename) {
-        new File(filename).getParentFile().mkdirs()
-        project.logger.lifecycle("Downloading ${url}")
-        def redirectLimit = 10
+    def downloadFollowingRedirect(String url, String filename, String releaseVersionString, String buildNumberString) {
+        Path targetPath=Paths.get(filename)
+        println("Target path: "+targetPath.toAbsolutePath())
+        Path targetDir=targetPath.getParent()
 
-        while(url) {
-            new URL(url).openConnection(proxy).with { conn ->
-                conn.instanceFollowRedirects = false
-                url = conn.getHeaderField( "Location" )
-                if(!url) {
-                    new File(filename).withOutputStream { out ->
-                        conn.inputStream.with { inp ->
-                            out << inp
-                            inp.close()
+        Files.createDirectories(targetDir) //new File(filename).getParentFile().mkdirs()
+        String versionFileName=appName.get()+"_"+releaseVersionString+"_"+buildNumberString+".apk"
+        Path versionPath=targetDir.resolve(versionFileName)
+
+        //TODO add forceDownload flag
+        if(Files.notExists(versionPath)) {
+            project.logger.lifecycle("Downloading ${url}")
+            def redirectLimit = 10
+
+            while (url) {
+                new URL(url).openConnection(proxy).with { conn ->
+                    conn.instanceFollowRedirects = false
+                    url = conn.getHeaderField("Location")
+                    if (!url) {
+                        versionPath.withOutputStream { out ->
+                            conn.inputStream.with { inp ->
+                                out << inp
+                                inp.close()
+                            }
                         }
                     }
                 }
+                redirectLimit--
+                if (redirectLimit == 0) {
+                    throw new GradleException("Url have more than 10 redirects.")
+                }
             }
-            redirectLimit--
-            if (redirectLimit == 0) {
-                throw new GradleException("Url have more than 10 redirects.")
-            }
+        } else {
+            println("Using existing app file")
         }
+        Files.copy(versionPath, targetPath, StandardCopyOption.REPLACE_EXISTING);
     }
 }
 
